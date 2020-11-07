56a7823ff3ebbd604da16587e61e0ee306df7e4f
nikos <ngalanis99@gmail.com>
2020-11-07 20:53:43 +0200

    Arguments + Hyperparameters parsing

--- Autoencoder/autoencoder.py
+++ Autoencoder/autoencoder.py
@@ -8,13 +8,17 @@ from common.mnist_parser import *
 
 
 parser = argparse.ArgumentParser(description='Create a NN for autoencoding a set of images')
-parser.add_argument('dataset', metavar='-d', type=str, nargs='+', help='Path of the dataset')
+
+parser.add_argument('-d', '--dataset', action='store', default=None,  metavar='', help='Relative path to the dataset')
 
 args = parser.parse_args()
 
-dataset_path = args.dataset[0]
-
-dataset = parse_X(dataset_path)
-# dataset.head()
+dataset = parse_X(args.dataset)
 
 print(dataset[:4])
+
+conv_layers = input("Give the number of convolution layers")
+conv_filter_size = input("Give the size of each convolution filter")
+n_conv_filters_per_layer = input("Give the number of convolution filters per layer")
+epochs = input("Give the number of epochs")
+batch_size = input("Give the batch size")
