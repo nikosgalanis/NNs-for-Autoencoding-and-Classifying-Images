b48cc668cd859b7d8da7f2e69408da94b6f85153
nikos <ngalanis99@gmail.com>
2020-11-17 18:59:11 +0200

    Classifier works perfectly. Just some tweaks todo

--- Classifier/classification.py
+++ Classifier/classification.py
@@ -36,6 +36,7 @@ from keras.backend import flatten
 from matplotlib import pyplot as plt
 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 
 from common.mnist_parser import *
@@ -71,11 +72,8 @@ def main():
 	test_X, _, _ = parse_X(args.testset)
 	test_Y = parse_Y(args.testlabels)
 
-	train_Y = to_categorical(train_Y)
-	test_Y = to_categorical(test_Y)
-
-	print("o sofo einai malakas ", train_Y.shape)
-
+	new_train_Y = to_categorical(train_Y)
+	new_test_Y = to_categorical(test_Y)
 	# normalize all values between 0 and 1 
 	train_X = train_X.astype('float32') / 255.
 	test_X = test_X.astype('float32') / 255.
@@ -84,7 +82,7 @@ def main():
 	train_X = np.reshape(train_X, (len(train_X), rows, columns, 1))
 	test_X = np.reshape(test_X, (len(test_X), rows, columns, 1))
 
-	train_X ,valid_X, train_ground, valid_ground = train_test_split(train_X, train_Y, test_size=0.2, random_state=42)
+	train_X ,valid_X, train_ground, valid_ground = train_test_split(train_X, new_train_Y, test_size=0.2, random_state=42)
 
 
 	loaded_encoder = load_model(args.model)
@@ -104,6 +102,8 @@ def main():
 	
 	encode = loaded_encoder(input_img)
 
+	# create an empty list in order to store the models
+	models_list = []
 
 	while 1:
 			choice = int(input("Choose one of the following options to procced: \n \
@@ -118,12 +118,12 @@ def main():
 				full_model = Model(input_img, fully_connected(encode, fc_n_neurons))
 				# 2 steps of training: 1st one, train only the fc layer
 				for layer in full_model.layers:
-					if (layer.name != 'dense'):
+					if (layer.name not in "dense"):
 						layer.trainable = False
 
 				full_model.compile(loss=categorical_crossentropy, optimizer=Adam(),metrics=['accuracy'])
 				full_model.summary()
-
+		
 				classify_train = full_model.fit(train_X, train_ground, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(valid_X, valid_ground))
 
 				# # 2nd step, train every layer
@@ -132,19 +132,82 @@ def main():
 
 				full_model.compile(loss=categorical_crossentropy, optimizer=Adam(),metrics=['accuracy'])
 				
-				classify_train = full_model.fit(train_X, train_Y, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(test_X, test_Y))
+				classify_train = full_model.fit(train_X, train_ground, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(valid_X, valid_ground))
 
-				full_model.save( "../Models/classifier.h5", save_format='h5')
+				model_plus_info = (full_model, epochs, batch_size, fc_n_neurons)
+				models_list.append(model_plus_info)
+				name = str(epochs) + "_" + str(batch_size) + "_" + str(fc_n_neurons) + ".h5"
 
-				break
+				full_model.save(name, save_format='h5')
+
 
 			elif (choice == 2):
-    				break;
+
+				model, epochs, batch_size, fc_n_neurons = models_list[-1]
+
+				accuracy = model.history.history['accuracy']
+				val_accuracy = model.history.history['val_accuracy']
+				loss = model.history.history['loss']
+				val_loss = model.history.history['val_loss']
+				epochs = range(len(accuracy))
+
+				plt.plot(epochs, accuracy, 'r', label='Training accuracy')
+				plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+				plt.title('Training and validation accuracy')
+				plt.legend()
+				plt.figure()
+
+				plt.plot(epochs, loss, 'r', label='Training loss')
+				plt.plot(epochs, val_loss, 'b', label='Validation loss')
+				plt.title('Training and validation loss')
+				plt.legend()
+				plt.show()
+
+				#TODO: plot ws pros tis yperparametrous
 			elif (choice == 3):
-    				break;
+				print("Select the trained model that you want to use")
+				i = 0
+				for (_, epochs, batch_size, fc_n_neurons) in models_list:
+					print(i, "- epochs: ", epochs, ", batchsize: ", batch_size, " n. neurons: ", fc_n_neurons, "\n")
+					i += 1
+				
+				selection = int(input())
+				model, _, _, _ = models_list[selection]
+
+				test_eval = model.evaluate(test_X, new_test_Y, verbose=0)
+
+				print('Test loss:', test_eval[0])
+				print('Test accuracy:', test_eval[1])
+
+		
+				predicted_classes = model.predict(test_X)
+				predicted_classes = np.argmax(np.round(predicted_classes),axis=1)
+				
+				# Print some correctly predicted images
+				correct = np.where(predicted_classes==test_Y)[0]
+				print ("Found " ,len(correct), " correct labels")
+				for i, correct in enumerate(correct[:9]):
+					plt.subplot(3,3,i+1)
+					plt.imshow(test_X[correct].reshape(28,28), cmap='gray', interpolation='none')
+					plt.title("Predicted {}, Class {}".format(predicted_classes[correct], int(test_Y[correct])))
+					plt.tight_layout()
+					plt.show()
+
+				# Print some incorrectly predicted images
+				incorrect = np.where(predicted_classes!=test_Y)[0]
+				print ("Found ", len(incorrect), " incorrect labels")
+				for i, incorrect in enumerate(incorrect[:9]):
+					plt.subplot(3,3,i+1)
+					plt.imshow(test_X[incorrect].reshape(28,28), cmap='gray', interpolation='none')
+					plt.title("Predicted {}, Class {}".format(predicted_classes[incorrect], int(test_Y[incorrect])))
+					plt.tight_layout()
+					plt.show()
+
+				target_names = ["Class {}".format(i) for i in range(10)]
+				print(classification_report(test_Y, predicted_classes, target_names=target_names))
+
 			else:
 				print("Choose one of the default values")
-				continue
 
 
 if __name__ == '__main__':
