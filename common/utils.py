bbff532a96eb992f760c9a619f90f283445ff98a
Sofo <46571975+SofoSt@users.noreply.github.com>
2020-11-18 15:56:24 +0200

    small problem in list difference

--- common/utils.py
+++ common/utils.py
@@ -1,17 +1,17 @@
 def list_difference(l1, l2):
     diff = 0
     index = -1
-    for i,(x,y) in enumerate(zip(l1,l2)):
+    for i, (x, y) in enumerate(zip(l1, l2)):
         if x != y:
             diff += 1
             index = i
-    
-    return (diff,i)
+
+    return (diff, index)
 
 
-l1 = (1,2,3)
-l2 = (1,2,4)
+l1 = (1, 2, 3)
+l2 = (1, 2, 4)
 
-print(list_difference(l1,l2))
+print(list_difference(l1, l2))
 
-print (l1[1:])
+print(l1[1:])
